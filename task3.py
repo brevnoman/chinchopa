bddd08f8c7e5c956454a019d6c7969376da1c42a
Sautin Dmytro <dimonsova1488@gmail.com>
2021-04-15 21:25:24 +0300

    a

---
+++ task3.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+
+s = '#'
+p = '#\t\t#'
+print(s*9, p, p, p, s*9, "\n", p, p, s*9, p, p, sep="\n")
