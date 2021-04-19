b25fec282bc77ae06d8a6c77ad582d370a1edbe6
Sautin Dmytro <dimonsova1488@gmail.com>
2021-04-19 19:53:54 +0300

    lesson3

--- Lesson3/Task1.py
+++ Lesson3/Task1.py
@@ -7,7 +7,6 @@ while str_len < 2:
     changeable_string = str(input())
     str_len = len(changeable_string)
 
-changeable_string = changeable_string+" "
 first_2let = changeable_string[0:2]
-last_2let = changeable_string[-3:-1]
+last_2let = changeable_string[-2]+changeable_string[-1]
 print(first_2let+last_2let)
