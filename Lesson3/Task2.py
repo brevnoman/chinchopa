8e5c23076d476ae3c6ec45ccaa65d7c6318ba2a6
Sautin Dmytro <dimonsova1488@gmail.com>
2021-04-19 19:42:41 +0300

    lesson3

--- Lesson3/Task2.py
+++ Lesson3/Task2.py
@@ -2,7 +2,6 @@
 
 
 ph_num = str(input("input your phone number 10 digits:"))
-first_dig=ph_num[0]
 while len(ph_num) !=10:
     ph_num=str(input("Please Try Again2:"))
     while ph_num.isdigit() !=True:
