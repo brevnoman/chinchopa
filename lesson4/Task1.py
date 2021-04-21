51409d660f4df41977152e5c6a7288ea254a0e46
Sautin Dmytro <dimonsova1488@gmail.com>
2021-04-21 22:34:12 +0300

    lesson4

--- lesson4/Task1.py
+++ lesson4/Task1.py
@@ -2,7 +2,8 @@ import random
 
 while True:
     your_try = input("enter a number from 1 to 10, lets see your luck: ")
-    if random.randrange(1, 11).__str__() == your_try.__str__():
+    #if random.randrange(1, 11).__str__() == your_try.__str__():
+    if "1" == your_try.__str__():
         while True:
             choise = input("you are lucky today, would you like to try one more time y/n: ")
             if choise == "y":
@@ -13,7 +14,7 @@ while True:
                 print('you can chose only "y" or "n": ')
                 continue
 
-    else:
+    elif str.isdigit(your_try) and int(your_try) in range(1,11):
         while True:
             choise = input("mb next time, want try one more time? y/n: ")
             if choise == "y":
@@ -24,6 +25,8 @@ while True:
                 print('you can chose only "y" or "n": ')
                 continue
 
+    else:
+        print("You should choose only numbers from 1 to 10")
 
 # более чистый вариант для консоли
 
