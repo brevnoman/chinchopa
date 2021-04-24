9a9a2151f19a41b381c196abdc087716c4f7547c
Sautin Dmytro <dimonsova1488@gmail.com>
2021-04-24 10:26:55 +0300

    lesson5

--- Lesson5/Task3.py
+++ Lesson5/Task3.py
@@ -4,6 +4,6 @@ listind=0   #index of "l" list
 while listind<100:          #cycle to get 100 indeces and check every element
     c= l[listind]       #current element of "l" list
     listind += 1
-    if c%7==0 and not c%5==0:   #check multiplicity to 7 and non-multiplicity to 5
+    if c%7==0 and c%5!=0:   #check multiplicity to 7 and non-multiplicity to 5
         b.append(c)     #adding to "b" list suitable elements
 print(b)
