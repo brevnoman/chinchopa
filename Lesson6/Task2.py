68df12bc3514463475770a9309873d8984bf1797
Sautin Dmytro <dimonsova1488@gmail.com>
2021-04-26 00:37:42 +0300

    lesson6

---
+++ Lesson6/Task2.py
@@ -0,0 +1,14 @@
+from prettytable import PrettyTable
+stock = {"banana": 6,"apple": 0,"orange": 32,"pear": 15}
+prices = {"banana": 4,"apple": 2,"orange": 1.5,"pear": 3}
+p=list(stock.keys())
+o=list(prices.keys())
+p1=list(stock.values())
+o1=list(prices.values())
+print(o)
+print(p)
+table=PrettyTable()
+table.field_names=["Title:", "Amount:", "Price for one piece:", "Cost:"]
+for i in range(len(p)):
+    table.add_row([p[i], p1[i], o1[i], p1[i]*o1[i]])
+print(table)
