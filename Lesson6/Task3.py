5e6fb39528924fc112be3a0084f473aebd04d45e
Sautin Dmytro <dimonsova1488@gmail.com>
2021-04-26 21:57:30 +0300

    lesson6

--- Lesson6/Task3.py
+++ Lesson6/Task3.py
@@ -14,7 +14,9 @@
 #spisok=[tuple(i for i in range(1,11)),tuple(i*i for i in range(1,11))]
 #print (spisok)
 # 3 Variant
-list1 = []
-for i in range(1, 11):
-    list1.append((i, i**2))
-print(list1)
+# list1 = []
+# for i in range(1, 11):
+#     list1.append((i, i**2))
+list2=list((i,i**2) for i in range(1,11))
+print(list2)
+
