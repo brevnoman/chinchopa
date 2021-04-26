b5055e2ee1d7104714b566714615379a6f17c492
Sautin Dmytro <dimonsova1488@gmail.com>
2021-04-26 20:57:47 +0300

    lesson6

--- Lesson6/Task2.py
+++ Lesson6/Task2.py
@@ -1,17 +1,15 @@
 from prettytable import PrettyTable
 stock = {"banana": 6,"apple": 0,"orange": 32,"pear": 15}
 prices = {"banana": 4,"apple": 2,"orange": 1.5,"pear": 3}
-p=list(stock.keys())
-o=list(prices.keys())
-p1=list(stock.values())
-o1=list(prices.values())
-print(o)
-print(p)
+stocknames=list(stock.keys())
+pricesnames=list(prices.keys())
+stocknum=list(stock.values())
+pricesnum=list(prices.values())
 total=0
 table=PrettyTable()
 table.field_names=["Title:", "Amount:", "Price for one piece:", "Cost:"]
-for i in range(len(p)):
-    table.add_row([p[i], p1[i], o1[i], p1[i]*o1[i]])
-    total+=p1[i]*o1[i]
+for i in range(len(stocknames)):
+    table.add_row([stocknames[i], stocknum[i], pricesnum[i], stocknum[i]*pricesnum[i]])
+    total+=stocknum[i]*pricesnum[i]
 print(table)
 print("Total price:",total)
