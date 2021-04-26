2f6f96c09d039a0875594c2605cfd0fac634fcc4
Sautin Dmytro <dimonsova1488@gmail.com>
2021-04-26 20:44:08 +0300

    lesson6

--- Lesson6/Task3.py
+++ Lesson6/Task3.py
@@ -1,10 +1,15 @@
-i=tuple(range(1,11))
-j=[]
-for p in i:
-    d=p**2
-    j.append(d)
-j=tuple(j)
-spisok=[i,j]
-print(i)
-print(j)
-print(spisok)
+# 1 Variant
+# i=tuple(range(1,11))
+# j=[]
+# for p in i:
+#     d=p**2
+#     j.append(d)
+# j=tuple(j)
+# spisok=[i,j]
+# print(i)
+# print(j)
+# print(spisok)
+
+# 2 Variant
+spisok=[tuple(i for i in range(1,11)),tuple(i*i for i in range(1,11))]
+print (spisok)
