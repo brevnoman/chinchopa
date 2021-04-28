f65a8d29b8f449edbcb34cc17e7f750ce5318f94
Sautin Dmytro <dimonsova1488@gmail.com>
2021-04-28 22:13:20 +0300

    Lesson7

--- Lesson7/Task2.py
+++ Lesson7/Task2.py
@@ -38,3 +38,49 @@ def main():
 
 
 main()
+
+
+#
+# countries = {}
+#
+#
+#
+# def make_country():
+#     country = input("name the country\n").lower()
+#     capital = input(f"name {country[0].upper()+country[1:]} capital?\n").lower()
+#     upper_country = country[0].upper() + country[1:]
+#     upper_capital=capital[0].upper() + capital[1:]
+#     countries.setdefault(upper_country, upper_capital)
+#     if capital != str(countries[upper_country]).lower():
+#         while True:
+#             want_change = input(f"last time you said it was {countries[upper_country]}, do you want to change it?")
+#             if want_change == "yes":
+#                 countries[upper_country] = upper_capital
+#                 break
+#             elif want_change == "no":
+#                 print("ok")
+#                 break
+#             else:
+#                 print('you should choose only from "yes" or "no"')
+#
+#
+# def want_out():
+#     while True:
+#         choose = input("Want you start over?(yes/no)")
+#         if choose == 'yes':
+#             main()
+#             break
+#         elif choose == 'no':
+#             print("bye")
+#             break
+#         else:
+#             print('You can use only "yes" or "no"')
+#
+#
+# def main():
+#     make_country()
+#     print(countries)
+#     want_out()
+#
+#
+# main()
