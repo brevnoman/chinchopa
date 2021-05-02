59bf6cd435d070c187cefa1d5923d5c0d350f4a1
Sautin Dmytro <dimonsova1488@gmail.com>
2021-05-02 13:53:14 +0300

    Lesson9

--- Lesson9/Task1.py
+++ Lesson9/Task1.py
@@ -1,15 +1,50 @@
 import json
 
 
-def write_file(your_string="Heello json World!!!"):
+def write_file(data):
+
     with open("myfile.txt", "w") as write:
-        json.dump(your_string, write)
+        # json.dump(your_string+" ", write)
+        write.write(json.dumps(''.join(data)))
+
+
 def open_file():
     with open("myfile.txt", "r") as read:
         reader = json.load(read)
         print(reader)
+
+
 def main():
-    write_file()
-    open_file()
+    try:
+        with open("myfile.txt", "r") as f:
+            data = [json.load(f)]
+    except Exception:
+        with open("myfile.txt", "r") as f:
+            f.write(json.dumps(f))
+    while True:
+        with open("myfile.txt", "r") as f:
+            data = [json.load(f)]
+        your_string = input()
+        data.append(''.join(your_string+" "))
+        write_file(data)
+        open_file()
+
 
 main()
+
+
+# import json
+#
+#
+# def write_file(your_string="Heello json World!!!"):
+#     with open("myfile.txt", "w") as write:
+#         json.dump(your_string, write)
+# def open_file():
+#     with open("myfile.txt", "r") as read:
+#         reader = json.load(read)
+#         print(reader)
+# def main():
+#     write_file()
+#     open_file()
+#
+# main()
