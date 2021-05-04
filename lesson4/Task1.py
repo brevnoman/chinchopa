8db42c1956b3670b3d5e68110da33dbbd43a3434
brevnoman <82524945+brevnoman@users.noreply.github.com>
2021-05-04 12:47:00 +0300

    Update Task1.py
    Lesson4

--- lesson4/Task1.py
+++ lesson4/Task1.py
@@ -1,4 +1,5 @@
 import random
+
 while True:
     your_try = input("enter a number from 1 to 10, lets see your luck: ")
     if random.randrange(1, 11).__str__() == your_try.__str__():
@@ -11,7 +12,8 @@ while True:
             else:
                 print('you can chose only "y" or "n": ')
                 continue
-    elif str.isdigit(your_try) and int(your_try) in range(1,11):
+
+    else:
         while True:
             choise = input("mb next time, want try one more time? y/n: ")
             if choise == "y":
@@ -21,5 +23,32 @@ while True:
             else:
                 print('you can chose only "y" or "n": ')
                 continue
-    else:
-        print("You should choose only numbers from 1 to 10")
+
+
+# более чистый вариант для консоли
+
+# while True:
+#     your_try = input("enter a number from 1 to 10, lets see your luck: ")
+#     if random.randrange(1, 11).__str__() == your_try.__str__():
+#         choise = input("you are lucky today, would you like to try one more time y/n: ")
+#         while True:
+#             choise = input()
+#             if choise == "y":
+#                 break
+#             elif choise == "n":
+#                 exit("ok bye)")
+#             else:
+#                 print('you can chose only "y" or "n": ')
+#                 continue
+#
+#     else:
+#       choise = input("you are lucky today, would you like to try one more time y/n: ")
+#         while True:
+#             choise = input()
+#             if choise == "y":
+#                 break
+#             elif choise == "n":
+#                 exit("ok bye)")
+#             else:
+#                 print('you can chose only "y" or "n": ')
+#                 continue
