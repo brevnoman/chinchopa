32df3408cd81db06015da47108fa996f7df177ba
brevnoman <82524945+brevnoman@users.noreply.github.com>
2021-05-05 18:32:24 +0300

    Update task2.1.py

--- task2.1.py
+++ task2.1.py
@@ -1,16 +1,14 @@
-
+  
+<<<<<<< HEAD
 #task1
 
 
 me="Dima"
 date="15.04"
-
-#task1
-
-me="Dima"
-date="15.04"
-
-print("Good day",me,"!",date, "is a perfect day to learn some python")
 =======
- day",me,"!",date, "is a perfect day to learn some python")
+#task1
 
+me="Dima"
+date="15.04"
+>>>>>>> dz/chinchopa
+print("Good day",me,"!",date, "is a perfect day to learn some python")
