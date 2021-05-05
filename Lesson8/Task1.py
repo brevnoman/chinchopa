1e894da549f514b519c07d91c3b7e47884329c8e
Sautin Dmytro <dimonsova1488@gmail.com>
2021-05-05 18:37:01 +0300

    Lesson8

--- Lesson8/Task1.py
+++ Lesson8/Task1.py
@@ -12,13 +12,13 @@ hop()
 
 #
 # def oops():
-#     l=[0]
-#     a=l[1]
+#     l={"pip": "install"}
+#     a=l["install"]
 #
 # def hop():
 #     try:
 #         oops()
 #     except KeyError:
-#         print("that list is not that large")
+#         print("This is incorrect key")
 #
 # hop()
