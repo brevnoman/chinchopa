a816bc3eeaced288ff10e7d4915f46fb7cbacef7
Sautin Dmytro <dimonsova1488@gmail.com>
2021-05-12 14:58:15 +0300

    Lesson12

---
+++ Lesson12/Task1.py
@@ -0,0 +1,34 @@
+class Cat:
+
+    def __init__(self):
+        pass
+    def talk(self):
+        print("meow")
+
+class Dog:
+    def __init__(self):
+        pass
+    def talk(self):
+        print("woof")
+
+class Furniture:
+    def __init__(self):
+        pass
+
+    def stand(self):
+        print("it's just stand")
+
+def make_it_talk(animal):
+    if isinstance(animal, Cat) or isinstance(animal, Dog):
+        animal.talk()
+    else:
+        print("this thing can't talk")
+
+
+
+cat_obj=Cat()
+dog_obj=Dog()
+furniture_obj=Furniture()
+make_it_talk(cat_obj)
+make_it_talk(dog_obj)
+make_it_talk(furniture_obj)
