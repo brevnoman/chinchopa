6d15f10b49cf8eae036bdfbc6d55926c6f3f19dd
Sautin Dmytro <dimonsova1488@gmail.com>
2021-05-14 19:18:15 +0300

    Lesson14

--- Lesson14/Task1.py
+++ Lesson14/Task1.py
@@ -1,8 +1,10 @@
 import inspect
 
+
 def logger(f):
     def func_logger(*args, **kwargs):
         print(f"{f.__name__}, called with {inspect.getargvalues(inspect.currentframe()).locals['args']} parameters")
+
     return func_logger
 
 
@@ -15,6 +17,28 @@ def add_nums(x, y):
 def square_all(*args):
     return [arg ** 2 for arg in args]
 
-list=[1,2,3,4,5]
-add_nums(4,5)
+
+list = [1, 2, 3, 4, 5]
+add_nums(4, 5)
 square_all(list)
+
+#
+# def logger2(f):
+#     def func_logger(arg1, arg2):
+#         print(f"{f.__name__}, called with {arg1, arg2} arguments")
+#     return func_logger
+#
+#
+# @logger2
+# def add_nums2(x, y):
+#     return x + y
+#
+#
+# @logger2
+# def square_all2(*args):
+#     return [arg ** 2 for arg in args]
+#
+#
+#
+# add_nums2(4, 5)
+# square_all2(list)
