3bc9478f73b247d194034f9474a033d22225e9f4
Sautin Dmytro <dimonsova1488@gmail.com>
2021-05-19 22:32:30 +0300

    Lesson14

--- Lesson14/Task2.py
+++ Lesson14/Task2.py
@@ -1,13 +1,15 @@
-def stop_words(f):
-    def censure(name):
-        new_result = f(name)
-        new_result = new_result.replace("pepsi", "*").replace("BMW", "*")
-        return new_result
+def stop_words(stop_words=list):
+    def go_go(f):
+        def censure(name):
+            new_result = f(name)
+            for i in stop_words:
+                new_result = new_result.replace(i, "*")
+            return new_result
+        return censure
+    return go_go
 
-    return censure
 
-
-@stop_words
+@stop_words(stop_words=["BMW", "pepsi"])
 def create_slogan(name: str) -> str:
     return f"{name} drinks pepsi in his brand new BMW!"
 
