a0fe83ae57ba3dafe6611ce7410234e89c524b29
Sautin Dmytro <dimonsova1488@gmail.com>
2021-05-24 18:34:29 +0300

    Lesson 16

--- Lesson16/Task2.py
+++ Lesson16/Task2.py
@@ -1,7 +1,7 @@
 class CustomRange:
     def __init__(self, begin=0, end=None, step=1):
-        self.counter = begin
-        self.end = end
+        self.counter = self.__condition2(begin, end)
+        self.end = self.__condition1(begin, end)
         self.step = step
 
     def __iter__(self):
@@ -15,7 +15,22 @@ class CustomRange:
             raise StopIteration
         return return_value
 
+    def __condition1(self, begin, end):
+        if end is None:
+            return begin
+        else:
+            return end
 
-c = CustomRange(0, 10)
+    def __condition2(self, begin, end):
+        if end is None:
+            return 0
+        else:
+            return begin
 
-print(list(c))
+
+
+c = CustomRange
+
+print(list(c(0,10)))
+print(list(c(10)))
+print(list(c(0,10,2)))
