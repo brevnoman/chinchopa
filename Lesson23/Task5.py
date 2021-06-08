0da23e1c1bb9e748196e5052f9cc909ca9b9237a
Sautin Dmytro <dimonsova1488@gmail.com>
2021-06-08 22:04:08 +0300

    Lesson23

--- Lesson23/Task5.py
+++ Lesson23/Task5.py
@@ -1,7 +1,9 @@
 def sum_of_digits(digit_string: str) -> int:
-    if len(digit_string) == 1:
-        return eval(digit_string)
-    else:
-        return eval(f"{digit_string[0]}+{sum_of_digits(digit_string[1:])}")
-
-print(sum_of_digits("11111"))
+    try:
+        if len(digit_string) == 1:
+            return eval(digit_string)
+        else:
+            return eval(f"{digit_string[0]}+{sum_of_digits(digit_string[1:])}")
+    except NameError:
+        raise NameError("only int")
+print(sum_of_digits("vova"))
