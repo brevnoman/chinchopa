3b73f4acc61ddcc9aa82ea761fdcacfe5f0269af
Sautin Dmytro <dimonsova1488@gmail.com>
2021-06-10 18:17:13 +0300

    Lesson23

--- Lesson23/Task2.py
+++ Lesson23/Task2.py
@@ -1,5 +1,5 @@
 def is_palindrome(looking_str: str, index: int = 0) -> bool:
-    revers_index=(len(looking_str)-(index+len(looking_str)+1))
+    revers_index = -index-1
     if index == round(len(looking_str)/2):
         return True
     elif looking_str[index] == looking_str[revers_index]:
