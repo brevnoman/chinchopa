8d800910a967f95cf1ff70db146f1e0bf3178081
Sautin Dmytro <dimonsova1488@gmail.com>
2021-07-20 19:48:50 +0300

    Lesson36

--- Lesson36/Task1/client.py
+++ Lesson36/Task1/client.py
@@ -18,19 +18,19 @@ def listen_chat(s):
             break
         print(data.decode('utf-8'))
 
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    while True:
-        print("you always can change your name using '/rename'\n")
-        username = input("write your username")
-        s.send(username.encode("utf-8"))
-        validate = s.recv(1024)
-        if validate == b"nope":
-            print("sry you can't choose this username")
-        elif validate == b" ":
-            break
-    pool = [print_to_chat, listen_chat]
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        for func in pool:
-            executor.submit(func, s)
+if __name__ == '__main__':
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        while True:
+            print("you always can change your name using '/rename'\n")
+            username = input("write your username")
+            s.send(username.encode("utf-8"))
+            validate = s.recv(1024)
+            if validate == b"nope":
+                print("sry you can't choose this username")
+            elif validate == b" ":
+                break
+        pool = [print_to_chat, listen_chat]
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            for func in pool:
+                executor.submit(func, s)
--- Lesson36/client.py
+++ Lesson36/client.py
@@ -1,6 +1,6 @@
 import socket
 import threading
-import time
+
 
 HOST = '127.0.0.1'
 PORT = 65432
@@ -11,22 +11,22 @@ def print_to_chat(s):
         chat = input()
         s.send(chat.encode("utf-8"))
 
+if __name__ == '__main__':
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        while True:
+            print("you always can change your name using '/rename'\n")
+            username = input("write your username")
+            s.send(username.encode("utf-8"))
+            validate = s.recv(1024)
+            if validate == b"nope":
+                print("sry you can't choose this username")
+            else:
+                break
+        threading.Thread(target=print_to_chat, args=(s,)).start()
+        while True:
+            data = s.recv(1024)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    while True:
-        print("you always can change your name using '/rename'\n")
-        username = input("write your username")
-        s.send(username.encode("utf-8"))
-        validate = s.recv(1024)
-        if validate == b"nope":
-            print("sry you can't choose this username")
-        else:
-            break
-    threading.Thread(target=print_to_chat, args=(s,)).start()
-    while True:
-        data = s.recv(1024)
-
-        if not data:
-            break
-        print(data.decode('utf-8'))
+            if not data:
+                break
+            print(data.decode('utf-8'))
